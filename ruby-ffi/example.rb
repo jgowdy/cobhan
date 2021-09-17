21d7acd2efd38c49785c911ca44f7127940e2db4
Marc Heiligers <mheiligers@godaddy.com>
2021-09-17 12:49:21 -0700

    Modulerize Ruby

--- ruby-ffi/example.rb
+++ ruby-ffi/example.rb
@@ -1,97 +1,27 @@
 # frozen_string_literal: true
 
-require 'ffi'
+require_relative 'libplugtest'
+require 'thread'
 
-UnsupportedPlatformError = Class.new(StandardError)
-
-LIBRARY_ROOT_PATH = '../output/'
-
-if FFI::Platform::OS == 'linux' && Dir.glob('/lib/libc.musl*').length.positive?
-  OS_PATH = 'linux-musl'
-  NEED_CHDIR = 1
-else
-  OS_PATHS = { 'linux' => 'linux', 'darwin' => 'macos', 'windows' => 'windows' }.freeze
-  OS_PATH = OS_PATHS[FFI::Platform::OS]
-  NEED_CHDIR = 0
-  raise UnsupportedPlatformError, "Unsupported operating system: #{FFI::Platform::OS}" unless OS_PATH
+class Test
+  include Libplugtest
 end
 
-EXTS = { 'linux' => 'so', 'darwin' => 'dylib', 'windows' => 'dll' }.freeze
-EXT = EXTS[FFI::Platform::OS]
+test = Test.new
 
-CPU_ARCHS = { 'x86_64' => 'amd64', 'aarch64' => 'arm64' }.freeze
-CPU_ARCH = CPU_ARCHS[FFI::Platform::ARCH]
-raise UnsupportedPlatformError, "Unsupported CPU: #{FFI::Platform::CPU_ARCH}" unless CPU_ARCH
-
-LIB_PATH = File.expand_path(File.join(LIBRARY_ROOT_PATH, OS_PATH, CPU_ARCH))
-
-module LibplugtestFfi
-  extend FFI::Library
-  
-  if NEED_CHDIR == 1
-    # Save current directory
-    old_dir = Dir.pwd
-
-    # Switch to library directory
-    Dir.chdir(LIB_PATH)
-  end
-
-  # NOTE: Absolute path is required here
-  ffi_lib File.join(LIB_PATH, "libplugtest.#{EXT}")
-
-  if NEED_CHDIR == 1
-    # Restore directory
-    Dir.chdir(old_dir)
-  end
-
-  attach_function :calculatePi, %i[int32 pointer int32], :int32
-  attach_function :sleepTest, [:int32], :void
-  attach_function :addInt32, %i[int32 int32], :int32
-  attach_function :addInt64, %i[int64 int64], :int64
-  attach_function :addDouble, %i[double double], :double
-
-  attach_function :toUpper, %i[pointer int32 pointer int32], :int32
+th = Thread.new do
+  puts 'start sleeping'
+  test.sleep_test 5
+  # sleep 5
+  puts 'done sleeping'
 end
 
-module Libplugtest
-  def calculate_pi(digits)
-    pi_buffer = FFI::MemoryPointer.new(1, digits + 1, false)
+puts test.calculate_pi(100)
+puts 'done pi'
 
-    result = LibplugtestFfi.calculatePi(digits, pi_buffer, pi_buffer.size)
-    raise 'Failed to calculate pi' if result.negative?
+puts test.add_int32(10, 10)
+puts test.add_int64(20, 20)
+puts test.add_double(1.2, 2.3)
+puts test.to_upper('Initial value')
 
-    pi_buffer.get_string(0, result)
-  end
-
-  def to_upper(input)
-    in_ptr = FFI::MemoryPointer.from_string(input)
-    out_ptr = FFI::MemoryPointer.new(1, in_ptr.size + 1, false)
-
-    result = LibplugtestFfi.toUpper(input, input.length, out_ptr, out_ptr.size)
-    raise 'Failed to convert toUpper' if result.negative?
-
-    out_ptr.get_string(0, result)
-  end
-
-  def sleep_test(seconds)
-    LibplugtestFfi.sleepTest(seconds)
-  end
-
-  def add_int32(x, y)
-    LibplugtestFfi.addInt32(x, y)
-  end
-
-  def add_int64(x, y)
-    LibplugtestFfi.addInt64(x, y)
-  end
-
-  def add_double(x, y)
-    LibplugtestFfi.addDouble(x, y)
-  end
-end
-
-#puts Libplugtest.calculate_pi(100)
-#puts Libplugtest.add_int32(10, 10)
-#puts Libplugtest.add_int64(20, 20)
-#puts Libplugtest.add_double(1.2, 2.3)
-#puts Libplugtest.to_upper('Initial value')
+th.join
