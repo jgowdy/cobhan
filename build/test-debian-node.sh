84e5b67a590c663f2da085d1d197417f05baaaee
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-09-16 21:24:04 -0700

    Add debian/buster tests for glibc based Linux

---
+++ build/test-debian-node.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DOCKER_BUILD_FLAGS='--rm'
+
+DOCKER=`which podman 2>/dev/null || echo docker`
+
+
+$DOCKER build $DOCKER_BUILD_FLAGS -t node-debian-demo - <Dockerfile.node-debian-demo
+
+$DOCKER run node-debian-demo
+
---
+++ build/test-debian-ruby.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DOCKER_BUILD_FLAGS='--rm'
+
+DOCKER=`which podman 2>/dev/null || echo docker`
+
+
+$DOCKER build $DOCKER_BUILD_FLAGS -t ruby-debian-demo - <Dockerfile.ruby-debian-demo
+
+$DOCKER run ruby-debian-demo
