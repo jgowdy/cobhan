0a08229eca5ad804357951f61af20569115de583
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-09-16 21:19:00 -0700

    Actually working debian and alpine, node and ruby

--- build/test-alpine-node.sh
+++ build/test-alpine-node.sh
@@ -7,4 +7,4 @@ DOCKER=`which podman 2>/dev/null || echo docker`
 
 $DOCKER build $DOCKER_BUILD_FLAGS -t node-alpine-demo - <Dockerfile.node-alpine-demo
 
-
+$DOCKER run node-alpine-demo
--- build/test-alpine-ruby.sh
+++ build/test-alpine-ruby.sh
@@ -7,4 +7,4 @@ DOCKER=`which podman 2>/dev/null || echo docker`
 
 $DOCKER build $DOCKER_BUILD_FLAGS -t ruby-alpine-demo - <Dockerfile.ruby-alpine-demo
 
-
+$DOCKER run ruby-alpine-demo
