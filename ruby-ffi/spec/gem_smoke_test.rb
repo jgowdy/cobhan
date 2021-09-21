2bc9d10539b5d4fcc813bae32568fccdae7a0a70
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2021-09-21 16:49:32 +0200

    Ruby gem builds per platform

--- ruby-ffi/spec/gem_smoke_test.rb
+++ ruby-ffi/spec/gem_smoke_test.rb
@@ -1,10 +1,8 @@
-puts build_info = `rake build`
-gem_path = build_info.match(/built to (\S+)\./)[1]
-puts `gem install #{gem_path}`
-
 require 'cobhan'
 require 'rspec'
+
 load 'spec/cobhan_spec.rb'
+
 RSpec::Core::Runner.invoke
 
 # NOTE: The following runs the specs against the source which is not what we want.
