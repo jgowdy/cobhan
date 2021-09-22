ecac83eb7fbca2d4a3033b4ed63d4f678a94fbc1
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-09-22 11:26:20 -0700

    Cleanup

--- java-jna-ffi/src/main/java/com/godaddy/Main.java
+++ java-jna-ffi/src/main/java/com/godaddy/Main.java
@@ -1,10 +1,10 @@
 package com.godaddy;
-import com.godaddy.Libplugtest;
 
 public class Main {
 
 
     public static void main(String[] args) {
+
         var libplugtest = new Libplugtest();
         System.out.println(libplugtest.toUpper("Initial value"));
     }
