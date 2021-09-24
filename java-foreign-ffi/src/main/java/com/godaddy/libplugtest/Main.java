abba5cb374e112e6a19f208c64361117b7d76251
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-09-24 08:35:48 -0700

    Building

--- java-foreign-ffi/src/main/java/com/godaddy/libplugtest/Main.java
+++ java-foreign-ffi/src/main/java/com/godaddy/libplugtest/Main.java
@@ -1,3 +1,9 @@
 package com.godaddy.libplugtest;
 
 
+public class Main {
+    public static void main(String[] args) {
+        var libplugtest = new Libplugtest();
+        System.out.println(libplugtest.toUpper("Initial value"));
+    }
+}
