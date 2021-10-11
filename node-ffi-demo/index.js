99ffbcc050384a82f7c1bb09f47c59b2745635e1
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-10-11 10:01:58 -0700

    Clean up JavaScript

--- node-ffi-demo/index.js
+++ node-ffi-demo/index.js
@@ -2,14 +2,10 @@
 
 const libplugtest = require('libplugtestjs');
 
-console.log(libplugtest.toUpperInGoPointerInputsBufferFrom('Initial value'));
-console.log(libplugtest.toUpperInGoPointerInputsBufferFromBufferAllocUnsafe('Initial value'));
-console.log(libplugtest.toUpperInGoPointerInputsCreateCString('Initial value'));
+console.log(libplugtest.toUpperInGo('Initial value'));
 
-console.log(libplugtest.toUpperInGoStringInputsBufferFrom('Initial value'));
-console.log(libplugtest.toUpperInGoStringInputsBufferFromBufferAllocUnsafe('Initial value'));
-console.log(libplugtest.toUpperInGoStringInputsCreateCString('Initial value'));
-console.log(libplugtest.toUpperInGoStringInputsPassStringDirectlyBufferAllocUnsafe('Initial value'));
+let output = libplugtest.filterJsonInGo({ test: 'foo', test2: 'kittens' } , 'foo');
+console.log(output);
 
 console.log(libplugtest.addInt32InGo(2.9, 2.0));
 console.log(libplugtest.addInt64InGo(2.9, 2.0));
