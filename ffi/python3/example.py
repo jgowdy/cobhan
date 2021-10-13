5a3bbbc540a900d22adb772b202f72dcbe5cc06e
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-10-13 12:11:45 -0700

    Fix naming

--- ffi/python3/example.py
+++ ffi/python3/example.py
@@ -1,9 +1,9 @@
 import os
 from libplugtest import Libplugtest
 
-#library_root_path = str(os.path.abspath(os.path.join(os.path.dirname(__file__),"../output")))
-library_root_path = 'target/debug/liblibplugtest.dylib'
-lib = Libplugtest.from_library_file(str(os.path.abspath(library_root_path)))
+library_root_path = str(os.path.abspath(os.path.join(os.path.dirname(__file__),"../output")))
+#library_root_path = 'target/debug/libplugtest.dylib'
+#lib = Libplugtest.from_library_file(str(os.path.abspath(library_root_path)))
 
 print(lib.to_upper('Initial value'))
 
