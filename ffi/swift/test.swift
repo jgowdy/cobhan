b5493211ee9b6189e7302c11b70f68fa46645713
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-10-13 20:58:30 -0700

    Progress on swift

--- ffi/swift/test.swift
+++ ffi/swift/test.swift
@@ -1,13 +1,24 @@
-#!/usr/bin/swift
+//
+//  main.swift
+//  libplugtest-console
+//
+//  Created by Jeremiah Gowdy on 10/13/21.
+//
+
+//#!/usr/bin/swift
+
+//NOTE: Requires Disable library validation in XCode projects for dlopen of unsigned dylib
 
 import Darwin
+let handle = dlopen("/Users/jgowdy/projects/cobhan/library/rust/libplugtest/target/debug/libplugtest.dylib", RTLD_NOW)
 
-let handle = dlopen("/Users/jgowdy/projects/cobhan/library/rust/libplugtest/target/debug/libplugtest.dylib", RTLD_NOW);
+let alignment = MemoryLayout<CChar>.alignment
 
 typealias addInt32Func = @convention(c) (Int32, Int32) -> Int32
 typealias addInt64Func = @convention(c) (Int64, Int64) -> Int64
 typealias addDoubleFunc = @convention(c) (Float64, Float64) -> Float64
 typealias sleepTestFunc = @convention(c) (Int32) -> Void
+typealias toUpperFunc = @convention(c) (UnsafePointer<CChar>, Int32, UnsafeMutableRawPointer, Int32) -> Int32
 
 var sym = dlsym(handle, "addInt32")
 let addInt32 = unsafeBitCast(sym, to: addInt32Func.self)
@@ -21,6 +32,9 @@ let addDouble = unsafeBitCast(sym, to: addDoubleFunc.self)
 sym = dlsym(handle, "sleepTest")
 let sleepTest = unsafeBitCast(sym, to: sleepTestFunc.self)
 
+sym = dlsym(handle, "toUpper")
+let toUpper = unsafeBitCast(sym, to: toUpperFunc.self)
+
 let resultInt32 = addInt32(1,2)
 print(resultInt32)
 
@@ -29,6 +43,23 @@ print(resultInt64)
 
 let resultDouble = addDouble(1.1, 2.2)
 print(resultDouble)
+
+let input = "Initial value"
+let inputLen = input.utf8.count
+let outputPtr = UnsafeMutableRawPointer.allocate(byteCount: inputLen,  alignment: alignment)
+defer { outputPtr.deallocate() }
+let result = toUpper(input, Int32(inputLen), outputPtr, Int32(inputLen))
+if result < 0 {
+    print ("toUpper failed")
+    abort()
+}
+
+let output = String(unsafeUninitializedCapacity: inputLen) { (bytes: UnsafeMutableBufferPointer<UInt8>) -> Int in
+    memcpy(bytes.baseAddress, outputPtr, inputLen)
+    return inputLen
+}
+
+print(output)
 
 print("Sleeping for 5 seconds")
 sleepTest(5)
