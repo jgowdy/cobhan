bbf0fbd70afd2bf03d360557c9a141ee77856279
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-10-20 16:25:52 -0700

    Drop C for now

--- library/c/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-[ "${CONSOLE:-0}" -eq "1" ] && COMPILE_FLAGS="-o libplugtest libplugtest-tests.c" || COMPILE_FLAGS="-shared -init _init -o libplugtest.so"
-cc -I/usr/local/include -I/usr/local/opt/apr/include/apr-1 -I/usr/local/opt/apr-util/libexec/include/apr-1 -l apr-1 -l aprutil-1 -l unistring ${COMPILE_FLAGS} -Wall libplugtest.c cJSON.c cobhan.c
