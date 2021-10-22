df1a8b35a5415c3cffeebb3e86a5b1170845208f
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-10-22 11:53:00 -0700

    Build - Add build scripts

---
+++ library/go/libplugtest/build-debug.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+
+case $(uname -s) in
+"Darwin")
+    CC=gcc CGO_ENABLED=1 go build -buildmode=c-shared -o output/libplugtest.dylib
+    ;;
+
+*)
+    LD_RUN_PATH=\$ORIGIN CGO_ENABLED=1 go build -compiler=gccgo -buildmode=c-shared -o /output/libplugtest.so
+    ;;
+esac
