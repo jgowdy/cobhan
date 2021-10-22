40b19054e3c86cff8ee814695724a220ce77cf5e
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-10-22 16:22:40 -0700

    Build - Update scripts

--- library/go/cobhan/alpine.sh
+++ library/go/cobhan/alpine.sh
@@ -2,6 +2,7 @@
 
 case $(uname -s) in
 "Darwin")
+    echo "Skipping alpine.sh due to Darwin/macOS"
     ;;
 *)
     docker build -f Dockerfile.alpine -t cobhan-go-alpine . && CID=$(docker create cobhan-go-alpine) && docker cp ${CID}:/output . && docker rm ${CID}
--- library/go/libplugtest/alpine.sh
+++ library/go/libplugtest/alpine.sh
@@ -2,6 +2,7 @@
 
 case $(uname -s) in
 "Darwin")
+    echo "Skipping alpine.sh due to Darwin/macOS"
     ;;
 *)
     docker build -f libplugtest/Dockerfile.alpine -t libplugtest-go-alpine . && CID=$(docker create libplugtest-go-alpine) && docker cp ${CID}:/output ./libplugtest && docker rm ${CID}
