36c6eb32f95b969b3955c82ad7ea9293a859a33b
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-10-22 15:52:07 -0700

    Go - Alpine docker build script without macOS

--- library/go/cobhan/alpine.sh
+++ library/go/cobhan/alpine.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-docker build -f Dockerfile.alpine -t cobhan-go-alpine . && CID=$(docker create cobhan-go-alpine) && docker cp ${CID}:/output . && docker rm ${CID}
+
+case $(uname -s) in
+"Darwin")
+    ;;
+*)
+    docker build -f Dockerfile.alpine -t cobhan-go-alpine . && CID=$(docker create cobhan-go-alpine) && docker cp ${CID}:/output . && docker rm ${CID}
+    ;;
+esac
+
