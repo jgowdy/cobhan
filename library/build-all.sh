15d8f38534b4ab026338a7f37412260c5ad7f553
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-10-26 11:55:05 -0700

    Drop buster

--- library/build-all.sh
+++ library/build-all.sh
@@ -7,8 +7,6 @@ rm -rf "${OUTPUT_DIR:-SAFE}"
 
 ./build-macos-go.sh
 ./build-macos-rust.sh
-./build-buster-go.sh
-./build-buster-rust.sh
 ./build-bullseye-go.sh
 ./build-bullseye-rust.sh
 ./build-alpine-go.sh
