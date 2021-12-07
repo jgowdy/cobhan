a5653249169bf0ed56bcb3fba2f98ec57d1b4113
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-12-07 11:44:28 -0800

    Add upload artifact script

---
+++ upload-artifact.sh
@@ -0,0 +1,7 @@
+#!/bin/env bash
+set -u
+echo "$GITHUB_TOKEN" >.githubtoken
+unset GITHUB_TOKEN
+gh auth login --with-token <.githubtoken
+rm .githubtoken
+gh release upload "$VERSION" "$1" --clobber
