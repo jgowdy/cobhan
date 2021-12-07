c8b8d93cb9328bceca4cb9f88265cdec7747ca10
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-12-07 10:18:45 -0800

    Clean up C#

--- ffi/dotnetcore3/CobhanDemoLib.cs
+++ ffi/dotnetcore3/CobhanDemoLib.cs
@@ -4,150 +4,153 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-public class CobhanDemoLib
+namespace dotnetcore3
 {
-    private delegate double AddDoubleDelegate(double x, double y);
-    private delegate Int64 AddInt64Delegate(Int64 x, Int64 y);
-    private delegate Int32 AddInt32Delegate(Int32 x, Int32 y);
-    private delegate void SleepTestDelegate(Int32 seconds);
-    private delegate Int32 CalculatePiDelegate(Int32 digits, IntPtr output, Int32 outputCapacity);
-    private delegate int ToUpperDelegate(IntPtr input, Int32 inputLen, IntPtr output, Int32 outputCapacity);
-
-    private static readonly AddInt32Delegate addInt32Delegate;
-    private static readonly AddInt64Delegate addInt64Delegate;
-    private static readonly AddDoubleDelegate addDoubleDelegate;
-    private static readonly SleepTestDelegate sleepTestDelegate;
-    private static readonly CalculatePiDelegate calculatePiDelegate;
-    private static readonly ToUpperDelegate toUpperDelegate;
-    private static readonly IntPtr hLibrary = IntPtr.Zero;
-
-    static CobhanDemoLib()
+    public class CobhanDemoLib
     {
-        string os,ext;
-        bool needsChdir = false;
+        private delegate double AddDoubleDelegate(double x, double y);
+        private delegate Int64 AddInt64Delegate(Int64 x, Int64 y);
+        private delegate Int32 AddInt32Delegate(Int32 x, Int32 y);
+        private delegate void SleepTestDelegate(Int32 seconds);
+        private delegate Int32 CalculatePiDelegate(Int32 digits, IntPtr output, Int32 outputCapacity);
+        private delegate int ToUpperDelegate(IntPtr input, Int32 inputLen, IntPtr output, Int32 outputCapacity);
 
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        private static readonly AddInt32Delegate addInt32Delegate;
+        private static readonly AddInt64Delegate addInt64Delegate;
+        private static readonly AddDoubleDelegate addDoubleDelegate;
+        private static readonly SleepTestDelegate sleepTestDelegate;
+        private static readonly CalculatePiDelegate calculatePiDelegate;
+        private static readonly ToUpperDelegate toUpperDelegate;
+        private static readonly IntPtr hLibrary = IntPtr.Zero;
+
+        static CobhanDemoLib()
         {
-            os = "windows";
-            ext = "dll";
-        }
-        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-        {
-            if(Directory.GetFiles("/lib", "libc.musl*").Length > 0)
+            string os,ext;
+            bool needsChdir = false;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                needsChdir = true;
+                os = "windows";
+                ext = "dll";
             }
-            os = "linux";
-            ext = "so";
-        }
-        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-        {
-            os = "macos";
-            ext = "dylib";
-        }
-        else
-        {
-            throw new Exception("Unsupported OS");
-        }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                if(Directory.GetFiles("/lib", "libc.musl*").Length > 0)
+                {
+                    needsChdir = true;
+                }
+                os = "linux";
+                ext = "so";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                os = "macos";
+                ext = "dylib";
+            }
+            else
+            {
+                throw new Exception("Unsupported OS");
+            }
 
-        string arch = RuntimeInformation.ProcessArchitecture switch
-        {
-            Architecture.Arm64 => "arm64",
-            Architecture.X64 => "amd64",
-            _ => throw new Exception("Unsupported CPU")
-        };
+            string arch = RuntimeInformation.ProcessArchitecture switch
+            {
+                Architecture.Arm64 => "arm64",
+                Architecture.X64 => "amd64",
+                _ => throw new Exception("Unsupported CPU")
+            };
 
-        string libraryBasePath = "../output/";
-        string libraryPath = Path.GetFullPath(Path.Join(libraryBasePath, os, arch));
+            string libraryBasePath = "../output/";
+            string libraryPath = Path.GetFullPath(Path.Join(libraryBasePath, os, arch));
 
-        string oldDir = string.Empty;
-        if(needsChdir)
-        {
-            oldDir = Directory.GetCurrentDirectory();
-            Directory.SetCurrentDirectory(libraryPath);
-        }
+            string oldDir = string.Empty;
+            if(needsChdir)
+            {
+                oldDir = Directory.GetCurrentDirectory();
+                Directory.SetCurrentDirectory(libraryPath);
+            }
 
-        hLibrary = NativeLibrary.Load(Path.Join(libraryPath, $"cobhan-demo-lib.{ext}"));
+            hLibrary = NativeLibrary.Load(Path.Join(libraryPath, $"cobhan-demo-lib.{ext}"));
 
-        if(needsChdir)
-        {
-            Directory.SetCurrentDirectory(oldDir);
-        }
+            if(needsChdir)
+            {
+                Directory.SetCurrentDirectory(oldDir);
+            }
 
-        IntPtr addr;
+            IntPtr addr;
 
-        addr = NativeLibrary.GetExport(hLibrary, "toUpper");
-        toUpperDelegate = Marshal.GetDelegateForFunctionPointer<ToUpperDelegate>(addr);
-        addr = NativeLibrary.GetExport(hLibrary, "addDouble");
-        addDoubleDelegate = Marshal.GetDelegateForFunctionPointer<AddDoubleDelegate>(addr);
-        addr = NativeLibrary.GetExport(hLibrary, "addInt32");
-        addInt32Delegate = Marshal.GetDelegateForFunctionPointer<AddInt32Delegate>(addr);
-        addr = NativeLibrary.GetExport(hLibrary, "addInt64");
-        addInt64Delegate = Marshal.GetDelegateForFunctionPointer<AddInt64Delegate>(addr);
-        addr = NativeLibrary.GetExport(hLibrary, "sleepTest");
-        sleepTestDelegate = Marshal.GetDelegateForFunctionPointer<SleepTestDelegate>(addr);
-        addr = NativeLibrary.GetExport(hLibrary, "calculatePi");
-        calculatePiDelegate = Marshal.GetDelegateForFunctionPointer<CalculatePiDelegate>(addr);
-    }
-
-    public string toUpper(string input)
-    {
-        var inputBytes = Encoding.UTF8.GetBytes(input);
-
-        var handle = GCHandle.Alloc(inputBytes, GCHandleType.Pinned);
-        var result = 0;
-        try
-        {
-            result = toUpperDelegate(handle.AddrOfPinnedObject(), inputBytes.Length, handle.AddrOfPinnedObject(), inputBytes.Length);
-        }
-        finally
-        {
-            handle.Free();
+            addr = NativeLibrary.GetExport(hLibrary, "toUpper");
+            toUpperDelegate = Marshal.GetDelegateForFunctionPointer<ToUpperDelegate>(addr);
+            addr = NativeLibrary.GetExport(hLibrary, "addDouble");
+            addDoubleDelegate = Marshal.GetDelegateForFunctionPointer<AddDoubleDelegate>(addr);
+            addr = NativeLibrary.GetExport(hLibrary, "addInt32");
+            addInt32Delegate = Marshal.GetDelegateForFunctionPointer<AddInt32Delegate>(addr);
+            addr = NativeLibrary.GetExport(hLibrary, "addInt64");
+            addInt64Delegate = Marshal.GetDelegateForFunctionPointer<AddInt64Delegate>(addr);
+            addr = NativeLibrary.GetExport(hLibrary, "sleepTest");
+            sleepTestDelegate = Marshal.GetDelegateForFunctionPointer<SleepTestDelegate>(addr);
+            addr = NativeLibrary.GetExport(hLibrary, "calculatePi");
+            calculatePiDelegate = Marshal.GetDelegateForFunctionPointer<CalculatePiDelegate>(addr);
         }
 
-        if (result < 0)
-            throw new Exception("toUpper failed");
-
-        return Encoding.UTF8.GetString(inputBytes, 0, result);
-    }
-
-    public double AddDouble(double x, double y)
-    {
-        return addDoubleDelegate(x, y);
-    }
-
-    public long AddInt64(long x, long y)
-    {
-        return addInt64Delegate(x, y);
-    }
-
-    public int AddInt32(int x, int y)
-    {
-        return addInt32Delegate(x, y);
-    }
-
-    public Task SleepTest(int seconds)
-    {
-        return Task.Run(() => sleepTestDelegate(seconds));
-    }
-
-    public string CalculatePi(int digits)
-    {
-        var outputBytes = new byte[digits+1];
-        var handle = GCHandle.Alloc(outputBytes, GCHandleType.Pinned);
-        var result = 0;
-        try
+        public string toUpper(string input)
         {
-            result = calculatePiDelegate(digits, handle.AddrOfPinnedObject(), outputBytes.Length);
-        }
-        finally
-        {
-            handle.Free();
+            var inputBytes = Encoding.UTF8.GetBytes(input);
+
+            var handle = GCHandle.Alloc(inputBytes, GCHandleType.Pinned);
+            int result;
+            try
+            {
+                result = toUpperDelegate(handle.AddrOfPinnedObject(), inputBytes.Length, handle.AddrOfPinnedObject(), inputBytes.Length);
+            }
+            finally
+            {
+                handle.Free();
+            }
+
+            if (result < 0)
+                throw new Exception("toUpper failed");
+
+            return Encoding.UTF8.GetString(inputBytes, 0, result);
         }
 
-        if (result < 0)
-            throw new Exception("calculatePi failed");
+        public double AddDouble(double x, double y)
+        {
+            return addDoubleDelegate(x, y);
+        }
 
-        return Encoding.UTF8.GetString(outputBytes, 0, result);
+        public long AddInt64(long x, long y)
+        {
+            return addInt64Delegate(x, y);
+        }
+
+        public int AddInt32(int x, int y)
+        {
+            return addInt32Delegate(x, y);
+        }
+
+        public Task SleepTest(int seconds)
+        {
+            return Task.Run(() => sleepTestDelegate(seconds));
+        }
+
+        public string CalculatePi(int digits)
+        {
+            var outputBytes = new byte[digits+1];
+            var handle = GCHandle.Alloc(outputBytes, GCHandleType.Pinned);
+            int result;
+            try
+            {
+                result = calculatePiDelegate(digits, handle.AddrOfPinnedObject(), outputBytes.Length);
+            }
+            finally
+            {
+                handle.Free();
+            }
+
+            if (result < 0)
+                throw new Exception("calculatePi failed");
+
+            return Encoding.UTF8.GetString(outputBytes, 0, result);
+        }
     }
 }
