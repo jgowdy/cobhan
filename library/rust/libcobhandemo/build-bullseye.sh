991598205eb4b588026ffa1db2275a51325e9778
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-12-07 15:35:48 -0800

    Add linux/arm64 builds

--- library/rust/libcobhandemo/build-bullseye.sh
+++ library/rust/libcobhandemo/build-bullseye.sh
@@ -12,7 +12,12 @@ if [ "${DEBUG:-0}" -eq "1" ]; then
     export DOCKER_BUILDKIT=0
 fi
 
-"${DOCKER_BIN}" build -f Dockerfile.bullseye -t ${TAG} ${CONTEXT_DIR}
-CID="$( ${DOCKER_BIN} create ${TAG} )"
-docker cp ${CID}:/libcobhandemo/output .
-docker rm ${CID}
+"${DOCKER_BIN}" build -f Dockerfile.bullseye --platform linux/amd64 -t ${TAG} ${CONTEXT_DIR}
+CID="$(${DOCKER_BIN} create ${TAG})"
+docker cp "${CID}:/libcobhandemo/output" .
+docker rm "${CID}"
+
+"${DOCKER_BIN}" build -f Dockerfile.bullseye --platform linux/arm64 -t ${TAG} ${CONTEXT_DIR}
+CID="$(${DOCKER_BIN} create ${TAG})"
+docker cp "${CID}:/libcobhandemo/output" .
+docker rm "${CID}"
