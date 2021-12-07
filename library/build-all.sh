8eeec6c460f621d938ffde954ed7e81beaec6f79
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-12-07 10:52:43 -0800

    Remove alpine

--- library/build-all.sh
+++ library/build-all.sh
@@ -4,10 +4,7 @@ set -e
 OUTPUT_DIR="$(pwd)/output"
 rm -rf "${OUTPUT_DIR:-SAFE}"
 
-
 ./build-macos-go.sh
 ./build-macos-rust.sh
 ./build-bullseye-go.sh
 ./build-bullseye-rust.sh
-./build-alpine-go.sh
-./build-alpine-rust.sh
