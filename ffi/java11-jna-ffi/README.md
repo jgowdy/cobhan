c541dbf124208e1b309cb8a899b6984dea0c8b44
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-12-08 08:50:48 -0800

    Delete ffi/java11-jna-ffi directory

--- ffi/java11-jna-ffi/README.md
+++
@@ -1,5 +0,0 @@
-## Links
-
-https://java-native-access.github.io/jna/4.2.1/
-
-https://github.com/java-native-access/jna
--- ffi/java11-jna-ffi/src/main/java/com/godaddy/Main.java
+++
@@ -1,8 +0,0 @@
-package com.godaddy;
-
-public class Main {
-    public static void main(String[] args) {
-        var cobhandemolib = new CobhanDemoLib();
-        System.out.println(cobhandemolib.toUpper("Initial value"));
-    }
-}
