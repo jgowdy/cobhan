5bf8b769fcc58d4952f38695ff9c8e41e957abf0
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-01-04 13:19:43 -0800

    Fix naming further

--- ffi/node14/consumer-console-app/index.js
+++ ffi/node14/consumer-console-app/index.js
@@ -1,19 +1,19 @@
-import { toUpperInGo, filterJsonObjectInGo, addInt32InGo, addInt64InGo, addDoubleInGo, sleepInGo } from 'cobhan-demo-lib';
+import { toUpper, filterJsonObject, addInt32, addInt64, addDouble, sleepTest } from 'cobhan-demo-lib';
 
-console.log(toUpperInGo('Initial value'));
+console.log(toUpper('Initial value'));
 
-let output = filterJsonObjectInGo({ test: 'foo', test2: 'kittens' } , 'foo');
+let output = filterJsonObject({ test: 'foo', test2: 'kittens' } , 'foo');
 console.log(output);
 
 // Intentionally showing integer behavior Number is truncation, not rounding
-console.log(addInt32InGo(2.9, 2.0));
-console.log(addInt64InGo(2.9, 2.0));
+console.log(addInt32(2.9, 2.0));
+console.log(addInt64(2.9, 2.0));
 
 // Double is the same as Number
-console.log(addDoubleInGo(2.9, 2.0));
+console.log(addDouble(2.9, 2.0));
 
 // Test using a Promise to call a blocking function
 console.log('Start sleeping');
-sleepInGo(2).then(() => {
+sleepTest(2).then(() => {
         console.log('Finished sleeping');
     })
